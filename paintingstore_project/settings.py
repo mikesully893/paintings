1b26f35626378c092c51aa367cd6ec68970a1880
mikesully893 <msully893@gmail.com>
2022-03-22 15:21:47 +0000

    Environment variables

--- paintingstore_project/settings.py
+++ paintingstore_project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v_21!kgq7msgr=@l$ipn0vah0%ckc#ilu^*3dr-42w4ibj%&%t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
