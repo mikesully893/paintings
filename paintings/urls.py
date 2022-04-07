980b0984805b0497409ec81e18b81ce7ec809d0a
mikesully893 <msully893@gmail.com>
2022-04-07 09:06:00 +0100

    Added search functionality

--- paintings/urls.py
+++ paintings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PaintingListView, PaintingDetailView
+from .views import PaintingListView, PaintingDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', PaintingListView.as_view(), name='painting_list'),
     path('<uuid:pk>', PaintingDetailView.as_view(), name='painting_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- paintings/views.py
+++ paintings/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import Painting
 
@@ -13,3 +14,15 @@ class PaintingDetailView(DetailView):
     model = Painting
     context_object_name = 'painting'
     template_name = 'paintings/painting_detail.html'
+
+
+class SearchResultsListView(ListView):
+    model = Painting
+    context_object_name = 'painting_list'
+    template_name = 'paintings/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Painting.objects.filter(
+            Q(title__icontains=query) | Q(artist__icontains=query)
+        )
